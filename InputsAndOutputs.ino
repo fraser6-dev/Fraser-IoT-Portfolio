2af9b3e021db8e7d0822a767bb1f0263841489a3
Fraser <101232416+fraser6-dev@users.noreply.github.com>
2023-09-14 12:17:30 +1000

    Update InputsAndOutputs.ino

--- InputsAndOutputs.ino
+++ InputsAndOutputs.ino
@@ -1,26 +1,29 @@
-float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+#define BUTTON_PIN 16
+#define LED_PIN    18  
 
-// the setup routine runs once when you press reset:
+int led_state = LOW;   //these are needed for our toggle
+int button_state;      
+int last_button_state;  
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  Serial.begin(9600);                
+  pinMode(BUTTON_PIN, INPUT_PULLUP); //Setup pins
+  pinMode(LED_PIN, OUTPUT);        
+
+  button_state = digitalRead(BUTTON_PIN); //Read the button if its pressed
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin GPIO36:
-  int analogValue = analogRead(36);
-  // Rescale to potentiometer's voltage (from 0V to 3.3V):
-  float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
+  last_button_state = button_state;  
+  button_state = digitalRead(BUTTON_PIN); //current button state is the button pin
 
-  // print out the value you read:
-  Serial.print("Analog: ");
-  Serial.print(analogValue);
-  Serial.print(", Voltage: ");
-  Serial.println(voltage);
-  delay(1000);
+  if (last_button_state == HIGH && button_state == LOW) {
+    Serial.println("The button is pressed");
+
+    // toggle state of LED
+    led_state = !led_state;
+
+    // control LED arccoding to the toggled state
+    digitalWrite(LED_PIN, led_state);
+  }
 }
-
-
